c393397527f35515649c89b4d4a7a287505e125f
ElRama55 <165962088+ElRama55@users.noreply.github.com>
2024-04-17 22:46:34 -0300

    Add files via upload

---
+++ TP_4/funciones.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    console.log("Número 1:", n1);
+    console.log("Número 2:", n2);
+    let resultado = n1 + n2;
+    console.log("Resultado:", resultado);
+    alert(resultado);
+}
